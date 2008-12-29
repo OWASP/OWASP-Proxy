b94e1e7c0d9b39e011d6c4b2f197dbba6af3bdaa
Rogan Dawes <rogan@dawes.za.net>
2008-12-29 09:39:52 +0002

    Minor changes to eliminate a warning

--- src/main/java/org/owasp/proxy/Main.java
+++ src/main/java/org/owasp/proxy/Main.java
@@ -29,7 +29,7 @@ public class Main {
 		System.err.println("'DIRECT' or 'PROXY server:port' or 'SOCKS server:port'");
 	}
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		if (args == null || (args.length != 2 && args.length != 3)) {
 			usage();
 			return;
@@ -46,67 +46,64 @@ public class Main {
 		if (args.length == 3) {
 			proxy = args[2];
 		}
-		try {
-			final Proxy upstream;
-			if ("DIRECT".equals(proxy)) {
-				upstream = Proxy.NO_PROXY;
-			} else {
-				Proxy.Type type = null;
-				if (proxy.startsWith("PROXY ")) {
-					type = Proxy.Type.HTTP;
-				} else if (proxy.startsWith("SOCKS ")) {
-					type = Proxy.Type.SOCKS;
-				} else 
-					throw new IllegalArgumentException("Unknown Proxy type: " + proxy);
-				proxy = proxy.substring(6); // "SOCKS " or "PROXY "
-				int c = proxy.indexOf(':');
-				if (c == -1)
-					throw new IllegalArgumentException("Illegal proxy address: " + proxy);
-				InetSocketAddress addr = new InetSocketAddress(proxy.substring(0,c), Integer.parseInt(proxy.substring(c+1)));
-				upstream = new Proxy(type, addr);
-			}
-			final ProxySelector ps = new ProxySelector() {
-				@Override
-				public void connectFailed(URI uri, SocketAddress sa, IOException ioe) {
-					System.err.println("Proxy connection failed! " + ioe.getLocalizedMessage());
-				}
 
-				@Override
-				public List<Proxy> select(URI uri) {
-					return Arrays.asList(upstream);
-				}
-			};
-			HttpClientFactory hcf = new DefaultHttpClientFactory() {
-				@Override
-				public HttpClient createHttpClient() {
-					HttpClient client = super.createHttpClient();
-					client.setProxySelector(ps);
-					return client;
-				}
-			};
-			ProxyMonitor lpm = new LoggingProxyMonitor();
-			CertificateProvider cp = new DefaultCertificateProvider();
-
-			Listener l = new Listener(httpPort);
-			l.setProxyMonitor(lpm);
-			l.setCertificateProvider(cp);
-			l.setHttpClientFactory(hcf);
-			l.start();
-
-			Listener sl = new SocksListener(socksPort);
-			sl.setProxyMonitor(lpm);
-			sl.setCertificateProvider(cp);
-			sl.setHttpClientFactory(hcf);
-			sl.start();
-
-			System.out.println("Http listener started on " + httpPort + ", SOCKS listener started on " + socksPort);
-			System.out.println("Press Enter to terminate");
-			new BufferedReader(new InputStreamReader(System.in)).readLine();
-			sl.stop();
-			l.stop();
-			System.out.println("Terminated");
-		} catch (Exception e) {
-			e.printStackTrace();
+		final Proxy upstream;
+		if ("DIRECT".equals(proxy)) {
+			upstream = Proxy.NO_PROXY;
+		} else {
+			Proxy.Type type = null;
+			if (proxy.startsWith("PROXY ")) {
+				type = Proxy.Type.HTTP;
+			} else if (proxy.startsWith("SOCKS ")) {
+				type = Proxy.Type.SOCKS;
+			} else 
+				throw new IllegalArgumentException("Unknown Proxy type: " + proxy);
+			proxy = proxy.substring(6); // "SOCKS " or "PROXY "
+			int c = proxy.indexOf(':');
+			if (c == -1)
+				throw new IllegalArgumentException("Illegal proxy address: " + proxy);
+			InetSocketAddress addr = new InetSocketAddress(proxy.substring(0,c), Integer.parseInt(proxy.substring(c+1)));
+			upstream = new Proxy(type, addr);
 		}
+		final ProxySelector ps = new ProxySelector() {
+			@Override
+			public void connectFailed(URI uri, SocketAddress sa, IOException ioe) {
+				System.err.println("Proxy connection failed! " + ioe.getLocalizedMessage());
+			}
+
+			@Override
+			public List<Proxy> select(URI uri) {
+				return Arrays.asList(upstream);
+			}
+		};
+		HttpClientFactory hcf = new DefaultHttpClientFactory() {
+			@Override
+			public HttpClient createHttpClient() {
+				HttpClient client = super.createHttpClient();
+				client.setProxySelector(ps);
+				return client;
+			}
+		};
+		ProxyMonitor lpm = new LoggingProxyMonitor();
+		CertificateProvider cp = new DefaultCertificateProvider();
+
+		Listener l = new Listener(httpPort);
+		l.setProxyMonitor(lpm);
+		l.setCertificateProvider(cp);
+		l.setHttpClientFactory(hcf);
+		l.start();
+
+		Listener sl = new SocksListener(socksPort);
+		sl.setProxyMonitor(lpm);
+		sl.setCertificateProvider(cp);
+		sl.setHttpClientFactory(hcf);
+		sl.start();
+
+		System.out.println("Http listener started on " + httpPort + ", SOCKS listener started on " + socksPort);
+		System.out.println("Press Enter to terminate");
+		new BufferedReader(new InputStreamReader(System.in)).readLine();
+		sl.stop();
+		l.stop();
+		System.out.println("Terminated");
 	}
 }
