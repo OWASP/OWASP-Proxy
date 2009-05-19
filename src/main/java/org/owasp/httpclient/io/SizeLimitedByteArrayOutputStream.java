bb697c152d5fdbfa8a76af514ee3b7052eb6833c
Rogan Dawes <rogan@dawes.za.net>
2009-05-19 12:23:50 +0200

    Allow the buffer to be hard limited to the specified max, or soft-limited
    A soft-limited buffer will continue to grow if writes are performed, but the
    overflow() method will be called each time. It is the responsibility of the 
    implementation to arrange for the writer to stop somehow. e.g. a RuntimeException

--- src/main/java/org/owasp/httpclient/io/SizeLimitedByteArrayOutputStream.java
+++ src/main/java/org/owasp/httpclient/io/SizeLimitedByteArrayOutputStream.java
@@ -7,9 +7,17 @@ public abstract class SizeLimitedByteArrayOutputStream extends
 
 	private int max;
 
+	private boolean hardLimit = true;
+
 	public SizeLimitedByteArrayOutputStream(int max) {
 		super();
 		this.max = max;
+	}
+
+	public SizeLimitedByteArrayOutputStream(int max, boolean hardLimit) {
+		super();
+		this.max = max;
+		this.hardLimit = hardLimit;
 	}
 
 	public SizeLimitedByteArrayOutputStream(int size, int max) {
@@ -17,9 +25,15 @@ public abstract class SizeLimitedByteArrayOutputStream extends
 		this.max = max;
 	}
 
+	public SizeLimitedByteArrayOutputStream(int size, int max, boolean hardLimit) {
+		super(size);
+		this.max = max;
+		this.hardLimit = hardLimit;
+	}
+
 	@Override
 	public void write(int b) {
-		if (count < max) {
+		if (count < max || !hardLimit) {
 			super.write(b);
 			if (count >= max)
 				overflow();
@@ -28,8 +42,9 @@ public abstract class SizeLimitedByteArrayOutputStream extends
 
 	@Override
 	public void write(byte[] b, int off, int len) {
-		if (count < max) {
-			len = Math.min(max - count, len);
+		if (count < max || !hardLimit) {
+			if (hardLimit)
+				len = Math.min(max - count, len);
 			super.write(b, off, len);
 			if (count >= max)
 				overflow();
