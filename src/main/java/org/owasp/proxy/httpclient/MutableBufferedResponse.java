b4fa1ea266f64ad64abc46b09511bf2fe817f19d
Rogan Dawes <rogan@dawes.za.net>
2009-10-08 16:38:21 +0200

    Add method comments to MutableBufferedResponse

--- src/main/java/org/owasp/proxy/httpclient/MutableBufferedResponse.java
+++ src/main/java/org/owasp/proxy/httpclient/MutableBufferedResponse.java
@@ -37,6 +37,14 @@ public interface MutableBufferedResponse extends MutableResponseHeader,
 			return content;
 		}
 
+		/**
+		 * this method automatically performs any necessary Chunked or Gzip
+		 * decoding on the message content required to obtain the actual entity
+		 * content.
+		 * 
+		 * The decoded content is cached using a weak reference to reduce the
+		 * need to perform repeated decoding operations
+		 */
 		public byte[] getDecodedContent() throws MessageFormatException {
 			if (content == null)
 				return null;
@@ -46,6 +54,13 @@ public interface MutableBufferedResponse extends MutableResponseHeader,
 			return decoded.get();
 		}
 
+		/**
+		 * This method automatically applies any chunked or gzip encoding
+		 * specified in the message headers before setting the message content.
+		 * 
+		 * The decoded content is cached using a WeakReference to reduce the
+		 * need to perform repeated decoding operations.
+		 */
 		public void setDecodedContent(byte[] content)
 				throws MessageFormatException {
 			if (content == null) {
