3ea6423b08cb6922915e084a35e3e034d2a9443b
Rogan Dawes <rogan@dawes.za.net>
2010-11-25 22:07:36 +0200

    LICENSE: Change license to LGPLv3

--- src/test/java/org/owasp/proxy/io/CopyInputStreamTest.java
+++ src/test/java/org/owasp/proxy/io/CopyInputStreamTest.java
@@ -1,22 +1,24 @@
 /*
- *  This file is part of the OWASP Proxy, a free intercepting HTTP proxy
- *  library.
- *  Copyright (C) 2008  Rogan Dawes <rogan@dawes.za.net>
- *
- *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License version 2 as 
- *  published by the Free Software Foundation.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License along
- *  with this program; if not, write to the Free Software Foundation, Inc.,
- *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ * This file is part of the OWASP Proxy, a free intercepting proxy library.
+ * Copyright (C) 2008-2010 Rogan Dawes <rogan@dawes.za.net>
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to:
+ * The Free Software Foundation, Inc., 
+ * 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  *
  */
+
 package org.owasp.proxy.io;
 
 import static org.junit.Assert.*;
