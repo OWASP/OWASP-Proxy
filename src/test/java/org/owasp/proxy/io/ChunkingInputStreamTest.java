5253a2098cb83387938a05f0d7c564c8bfb7804d
Steve Martin <sjmart1uk@yahoo.co.uk>
2013-11-02 14:11:15 +0000

    Prevent resource leakage.
    Close streams when no longer required.

--- src/test/java/org/owasp/proxy/io/ChunkingInputStreamTest.java
+++ src/test/java/org/owasp/proxy/io/ChunkingInputStreamTest.java
@@ -62,9 +62,7 @@ public class ChunkingInputStreamTest {
 		data = new byte[16385];
 		for (int i = 0; i < data.length; i++)
 			data[i] = (byte) (i % 256);
-		InputStream is = new ByteArrayInputStream(data);
-		is = new ChunkingInputStream(is);
-		is = new ChunkedInputStream(is);
+		InputStream is = new ChunkedInputStream(new ChunkingInputStream(new ByteArrayInputStream(data)));
 		byte[] buff = new byte[data.length + 1024];
 		int read = 0, got;
 		while ((got = is.read(buff, read, Math.min(1024, buff.length - read))) > -1) {
@@ -73,6 +71,7 @@ public class ChunkingInputStreamTest {
 		}
 		Assert.assertEquals(data.length, read);
 		compare(data, 0, buff, 0, read);
+		is.close();
 	}
 
 	private void compare(byte[] a, int ao, byte[] b, int bo, int len) {
